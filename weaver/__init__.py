9e8d4c63d59d1927adf0f104bc2d1a79aed7c7da
John Xia <john.danger.xia@gmail.com>
2019-10-29 08:19:57 -0400

    Stub /posts endpoint.
    Stubbing out the structure here. The timestamp is a string because I was
    lazy about the hash template - we'll fix it once we're actually using a
    database instead of a dictionary.
    
    Things I considered but didn't do:
    * multiple parents - maybe a mistake? Seems like we could probably be
      returning a list type here with little consequence.
    * sequence number in addition to timestamp, to maintain fixed ordering.
      I think the true ordering information is in the parent field, though
      maybe you could imagine 'parent' should represent a whole snapshot of
      the state...

--- weaver/__init__.py
+++ weaver/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
@@ -11,8 +11,28 @@ def create_app():
     except FileExistsError:
         pass
 
-    @app.route("/hello")
-    def hello():
-        return "Hello, world!"
+    @app.route("/posts", methods=["GET"])
+    def posts():
+        from datetime import datetime as dt
+        import hashlib
+        root = {"id": 0}
+        hash_template = "a:{author}p:{parent}b:{body}t:{timestamp}"
+        loomings = {
+            "author": "herman",
+            "parent": None,
+            "body": "# Moby Dick\n\n##Chapter 1. Loomings\n\nI'm Ishmael",
+            "timestamp": "2019-10-29T08:02:00-04:00"
+        }
+        md5 = hashlib.md5(hash_template.format(**loomings).encode("utf-8"))
+        loomings["md5"] = md5.hexdigest()
+        ishmael = {
+            "author": "ishmael",
+            "parent": loomings["md5"],
+            "body": "'Call me Ishmael', maybe?",
+            "timestamp": "2019-10-29T08:02:01-04:00"
+        }
+        md5 = hashlib.md5(hash_template.format(**ishmael).encode("utf-8"))
+        ishmael["md5"] = md5.hexdigest()
+        return jsonify([loomings, ishmael])
 
     return app
